e2f26d7760ac9b58249575ebcb9df569d8cedf25
m0nt4ld0 <mmontaldo@live.com>
2024-05-26 16:01:36 -0300

    Resuelve primer apartado (creacion de bdd y tablas)

---
+++ ddl/Peliculas.sql
@@ -0,0 +1,38 @@
+-- ----------------------------------------------------------------------------------------
+-- Autor/a:                      Mariela Montaldo
+-- Fecha:                        26/05/2024
+-- Descripción:                  Genera BDD para TP de curso JAVA - Full Stack - Com. 24100
+-- ----------------------------------------------------------------------------------------
+CREATE DATABASE 'Peliculas';
+
+create table movies (
+    id_movie int not null auto_increment,
+    nombre varchar(50) not null,
+    descripcion varchar(255),
+    genero varchar(255),
+    calificacion decimal(3,2) not null default 0, -- a que hace referencia?
+    año char(4),
+    estrellas tinyint not null default 0, -- toma solo valores discretos?
+    director varchar(50) not null,
+    primary key (id_movie)
+);
+
+create table directores (
+    id_director int not null auto_increment,
+    nombre varchar(50),
+    apellido varchar(50) not null,
+    edad char(3),
+    nacionalidad varchar(50),
+    primary key(id_director)
+);
+
+create table usuarios (
+    id_usuario int not null auto_increment,
+    nombre varchar(50),
+    apellido varchar(50),
+    email varchar(255) not null,
+    contraseña varchar(255) not null,
+    fecha_nac date,
+    pais varchar(50),
+    primary key(id_usuario)
+);
